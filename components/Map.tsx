eb7b482bcc9b415fe7b82ba86947155340bf0a46
Yeuda Shtern <yeudashtern@gmail.com>
2024-06-27 19:48:23 +0300

    fix bugs

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,17 +1,21 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import { LatLngExpression } from 'leaflet';
+import { MapContainerProps } from 'react-leaflet';
 
-interface MapProps {
+interface MapProps extends MapContainerProps {
   location: string;
   lat: number;
   lon: number;
 }
 
-export default function Map({ location, lat, lon }: MapProps) {
+export default function Map({ location, lat, lon, ...props }: MapProps) {
+  const center: LatLngExpression = [lat, lon];
+
   return (
-    <MapContainer center={[lat, lon]} zoom={13} style={{ height: '400px', width: '100%' }}>
+    <MapContainer center={center} zoom={13} style={{ height: '400px', width: '100%', borderRadius: '15px' }} {...props}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[lat, lon]}>
+      <Marker position={center}>
         <Popup>{location}</Popup>
       </Marker>
     </MapContainer>
